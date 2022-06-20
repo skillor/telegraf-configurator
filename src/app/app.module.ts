0e478188e4a1afcc0784b081c4cf0ef9576a1df4
skillor <skillor@gmx.net>
2022-06-20 18:00:16 +0200

    feat: load monaco from cdn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,26 @@ import { HomeComponent } from './pages/home/home.component';
 import { PluginListComponent } from './components/plugin-list/plugin-list.component';
 import { AdvancedConfigEditorComponent } from './components/advanced-config-editor/advanced-config-editor.component';
 import { FormsModule } from '@angular/forms';
-import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
+import { MonacoEditorModule, MONACO_PATH } from '@materia-ui/ngx-monaco-editor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    PluginListComponent,
-    AdvancedConfigEditorComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    MonacoEditorModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        PluginListComponent,
+        AdvancedConfigEditorComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        MonacoEditorModule,
+    ],
+    providers: [{
+        provide: MONACO_PATH,
+        useValue: 'https://unpkg.com/monaco-editor@0.33.0/min/vs'
+    }],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
