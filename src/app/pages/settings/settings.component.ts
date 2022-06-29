1ac4fd0c47fbb7e074982868b5c46e5183c9f7d3
skillor <skillor@gmx.net>
2022-06-29 13:05:13 +0200

    feat: import export settings

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -50,4 +50,59 @@ export class SettingsComponent implements OnInit {
     getSettings(): Setting[] {
         return Object.values(this.settings);
     }
+
+    jsonSettings(): {[key: string]: any} {
+        return Object.fromEntries(Object.values(this.settings).map(setting => [setting.key, setting.value]));
+    }
+
+    exportSettings(): void {
+        const a = document.createElement('a');
+        const file = new Blob([JSON.stringify(this.jsonSettings())], {type: 'text/plain'});
+        a.href = URL.createObjectURL(file);
+        a.download = 'settings-' + new Date().toLocaleDateString('en-CA') + '.json';
+        a.click();
+    }
+
+    importSettings(): void {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.json';
+        input.style.display = 'none';
+        document.body.appendChild(input);
+
+        const onEnd = () => {
+            document.body.removeChild(input);
+        };
+
+        input.onchange = () => {
+            if (!input.files || input.files.length == 0 || input.files[0].size > 250000000 || !FileReader) {
+                return onEnd();
+            }
+            const file = input.files[0];
+            const fileReader = new FileReader();
+            fileReader.onload = (e) => {
+                if (!e.target || !e.target.result) {
+                    return onEnd();
+                }
+                const content = e.target.result as string;
+                try {
+                    for (const [key, value] of Object.entries(JSON.parse(content))) {
+                        if (key in this.settings && typeof this.settings[key].value === typeof value) {
+                            this.settings[key].value = value;
+                            // (this.settings as any)[key].value = value;
+                            // this.save_setting(key, value, false, false, loadingSubject, false);
+                            // updateCount++;
+                        } else {
+                            console.warn('unknown key or type mismatch', key, value);
+                        }
+                    }
+                } catch (err) {
+                    console.error(err);
+                }
+                onEnd();
+            };
+            fileReader.readAsText(file);
+        };
+        input.click();
+    }
 }
