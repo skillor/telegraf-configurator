5133cb306140f2111add5c0e74819827dd26413a
skillor <skillor@gmx.net>
2022-06-29 17:20:21 +0200

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,ts}'],
+    content: [
+        './src/**/*.{html,ts}',
+    ],
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [
+        require("daisyui")
+    ],
     daisyui: {
         themes: [
             {
