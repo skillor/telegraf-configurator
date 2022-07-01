9b5657d77def8005ae2fa5a763d5f6b1dbb004e6
skillor <skillor@gmx.net>
2022-07-01 13:25:08 +0200

    feat: warning before unload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { SettingsService } from './shared/settings/settings.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+    @HostListener('window:beforeunload', ['$event'])
+    beforeunloadHandler() {
+        return false;
+    }
+
     constructor(
         public settingsService: SettingsService,
     ) { }
